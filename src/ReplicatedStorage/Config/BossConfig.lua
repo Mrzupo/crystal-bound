88b822f2ba353bdd50a62d015bc754bef2b42e74
Mrzupo <zupo0712@outlook.de>
2026-08-18 00:32:39 +0200

    Add Crystal Guardian boss configuration

---
+++ src/ReplicatedStorage/Config/BossConfig.lua
@@ -0,0 +1,18 @@
+return {
+	CrystalGuardian = {
+		DisplayName = "Crystal Guardian",
+		Health = 2500,
+		Level = 15,
+		XP = 1800,
+		Money = 1200,
+		Respawn = 90,
+		AggroRange = 65,
+		AttackRange = 9,
+		AttackDamage = 28,
+		AttackCooldown = 1.4,
+		AbilityCooldown = 6,
+		AbilityRadius = 14,
+		AbilityDamage = 45,
+		Drop = "GuardianCore",
+	},
+}
