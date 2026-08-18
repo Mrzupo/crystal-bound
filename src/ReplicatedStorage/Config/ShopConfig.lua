32ebf0a1f1fe5d014b0e89cc5e7d43ed90905fc5
Mrzupo <zupo0712@outlook.de>
2026-08-18 18:41:02 +0200

    Add shared shop configuration

---
+++ src/ReplicatedStorage/Config/ShopConfig.lua
@@ -0,0 +1,16 @@
+return {
+	Offers = {
+		HealthPotion = {
+			ItemId = "HealthPotion",
+			Price = 75,
+			MaxPerPurchase = 10,
+		},
+	},
+	SellOrder = {
+		"EmberShard",
+		"TidePearl",
+		"GaleFeather",
+		"GuardianCore",
+		"AncientShard",
+	},
+}
