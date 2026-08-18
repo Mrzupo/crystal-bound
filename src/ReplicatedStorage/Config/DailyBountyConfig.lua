5880e50c0e64fe7992debe64a5795e853393d6f0
Mrzupo <zupo0712@outlook.de>
2026-08-18 19:17:07 +0200

    add shared daily bounty configuration

---
+++ src/ReplicatedStorage/Config/DailyBountyConfig.lua
@@ -0,0 +1,9 @@
+return {
+	Goals = {
+		{ EnemyType = "Emberling", Goal = 8, RewardMoney = 120 },
+		{ EnemyType = "Tidecrawler", Goal = 6, RewardMoney = 180 },
+		{ EnemyType = "Galewisp", Goal = 5, RewardMoney = 250 },
+		{ EnemyType = "CrystalBat", Goal = 4, RewardMoney = 320 },
+		{ EnemyType = "AncientGolem", Goal = 2, RewardMoney = 450 },
+	}
+}
