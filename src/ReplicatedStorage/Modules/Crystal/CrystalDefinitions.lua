a4ab56d53861d19bd49e09cb023311d3a8eb066a
Mrzupo <zupo0712@outlook.de>
2026-08-18 18:25:48 +0200

    Remove unused legacy crystal definitions

--- src/ReplicatedStorage/Modules/Crystal/CrystalDefinitions.lua
+++
@@ -1,5 +0,0 @@
-return {
-	EMBER = { Id = "EMBER", Name = "Ember", Element = "Fire" },
-	TIDE = { Id = "TIDE", Name = "Tide", Element = "Water" },
-	GALE = { Id = "GALE", Name = "Gale", Element = "Wind" },
-}
