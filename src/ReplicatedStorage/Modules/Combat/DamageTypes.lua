1023bc03905995fc79c1eef1e52b2b5beadb679d
Mrzupo <zupo0712@outlook.de>
2026-08-18 15:35:01 +0200

    Register explicit combat damage types

--- src/ReplicatedStorage/Modules/Combat/DamageTypes.lua
+++ src/ReplicatedStorage/Modules/Combat/DamageTypes.lua
@@ -1,6 +1,8 @@
 return {
 	Physical = "Physical",
 	Crystal = "Crystal",
+	CrystalAbilitySplash = "CrystalAbilitySplash",
+	BossShockwave = "BossShockwave",
 	True = "True",
 	Environmental = "Environmental",
 }
