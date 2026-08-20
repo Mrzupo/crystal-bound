67320d0cf307e03f8dad79f05bc563cde1897fba
Mrzupo <zupo0712@outlook.de>
2026-08-21 00:30:38 +0200

    tighten server movement observation cap

---
+++ src/ReplicatedStorage/Config/MovementConfig.lua
@@ -0,0 +1,12 @@
+return {
+	BaseWalkSpeed = 16,
+	MinWalkSpeed = 6,
+	MaxWalkSpeedBonus = 20,
+	MinSlowMultiplier = 0.2,
+	MaxSlowMultiplier = 1,
+	MaxObservedSpeed = 48,
+	ExtraDistanceTolerance = 12,
+	GraceDuration = 0.6,
+	PositionCheckInterval = 0.15,
+	PortalArrivalTolerance = 18,
+}
